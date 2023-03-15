d625bf1aa1f49a229f2274ccda4d374afac350f6
ChaiTech123 <chaithrad180@gmail.com>
2023-03-15 16:44:54 +0530

    First Commit to Second RepoEclipse

---
+++ FirstApp/src/FirstClass.java
@@ -0,0 +1,65 @@
+
+class FirstClass {
+	static public void main(String args[])
+	{
+		/*
+		 * System.out.println("Hellp World"); System.out.println(args[0]);
+		 * System.out.println(args[1]);
+		 * 
+		 * System.out.println(Byte.MAX_VALUE); System.out.println(Byte.SIZE);
+		 * 
+		 * long l=10; long l2=1234567890890L; long Long=10L; long Long1=10; //long
+		 * l2=1234567890890;
+		 * 
+		 * byte a=10; byte b=20; //byte c=a+b;
+		 * 
+		 * int d1=25; int d2=2; float d3=d1/d2; System.out.println(d3);
+		 * 
+		 * byte var1=45; double var2=var1; System.out.println(var2);
+		 * 
+		 * double db=1234; byte bt=(byte)db; System.out.println(bt);
+		 * 
+		 * 
+		 * byte ab=20; byte dc=30; int btt=ab*dc; System.out.println(ab*dc);
+		 * 
+		 * int result=(int)12.0f; System.out.println(result);
+		 */
+		
+		//byte num=1;
+		
+		//byte num+=1;
+		
+		
+		/*
+		 * Boolean b1=true; boolean b2=false; boolean b3=true;
+		 * 
+		 * if((b1=false) |(b2&b3) &b3) { System.out.println("alpha"); } if((b1=false) |
+		 * (b1 &b3) | (b1|b2)) { System.out.println("beta"); }
+		 */
+		
+		/*
+		 * boolean b1=true; boolean b2=false;
+		 * 
+		 * System.out.println(!false ^ false); System.out.println(!b1 & (b2=true));
+		 * System.out.println(b2 ^ b1);
+		 */
+		
+		
+		Integer x=0;
+		Integer y=0;
+		
+		for(Short z=0;z<5;z++ )
+		      if((++x > 2) || (++y >2) )
+		    	  x++;
+		
+		System.out.println(x+ " "+y);
+		
+		/*
+		 * int z; if(args.length>0) z=10; System.out.println(z);
+		 *///local variable z may npt have been initialized
+	   
+		
+		
+	}
+
+}
